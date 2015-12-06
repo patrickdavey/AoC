c5dcb462db6017d8ec19cfcdb581fcad385fefa4
Patrick Davey <patrick.davey@gmail.com>
2015-12-06 13:26:55 +1300

    Basic hashing function

---
+++ day4/run.rb
@@ -0,0 +1,13 @@
+require 'pry'
+require 'digest'
+
+secret_key = 'iwrupvqb'
+modifier = -1
+result = ""
+
+loop do
+  modifier = modifier + 1
+  result = "#{secret_key}#{modifier}"
+  break if Digest::MD5.hexdigest(result).start_with?('00000')
+end
+puts result
