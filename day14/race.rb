f9d5495293efae03af664f728ede133c297eceab
Patrick Davey <patrick.davey@gmail.com>
2016-02-24 18:56:48 -0300

    winner method working

--- day14/race.rb
+++ day14/race.rb
@@ -1,12 +1,31 @@
 require 'pry'
-require './race.rb'
+require './reindeer.rb'
 
 class Race
+  PARSE = /^(?<name>\w+) .+ fly (?<speed>\d+) .+ for (?<duration>\d+) .+ for (?<rest>\d+)/
   def initialize(race_info)
-    #parse_race_info(race_info)
+    @entrants = []
+    parse_race_info(race_info)
   end
 
   def winner_after(number_of_seconds)
-    0
+    number_of_seconds.times do
+      @entrants.map(&:tick)
+    end
+
+    @entrants.max_by(&:distance)
+  end
+
+
+  private
+
+  def parse_race_info(race_info)
+    race_info.each_line do |line|
+      info = line.strip.match(PARSE)
+      @entrants << Reindeer.new(name: info[:name],
+                                speed: info[:speed].to_i,
+                                duration: info[:duration].to_i,
+                                rest: info[:rest].to_i)
+    end
   end
 end
