896e98c610d7eea382986d670f2460d01879f0ec
Patrick Davey <patrick.davey@gmail.com>
2016-03-17 14:57:53 -0400

    Part 1 complete of day 17.
    Boo-Ya .. still, must  be a nicer way

---
+++ day17/run.rb
@@ -0,0 +1,11 @@
+require "./filler.rb"
+require "pry"
+
+buckets = []
+IO.read("./input.txt").each_line do |bucket|
+  buckets << bucket.to_i
+end
+binding.pry
+filler = Filler.new(buckets, 150)
+
+puts filler.find_possible_fills
