445674bfa0854be3260fe8665edd6f5cae20e345
Patrick Davey <patrick.davey@gmail.com>
2016-03-17 14:54:40 -0400

    first test passing, I still find recursion weird

--- day17/filler.rb
+++ day17/filler.rb
@@ -7,22 +7,36 @@ class Filler
     @litres_to_fill = litres_to_fill
   end
 
-  def possible_fills(remaining_buckets = buckets.dup, remaining_to_fill = litres_to_fill)
-    return unless remaining_buckets.size > 0
+  def find_possible_fills
+    first_bucket = buckets.pop
+    remaining_to_fill = litres_to_fill - first_bucket
+    return possible_fills(buckets, remaining_to_fill)
+  end
 
-    remaining_to_fill = remaining_to_fill - remaining_buckets.pop
+  def possible_fills(remaining_buckets, remaining_to_fill)
+    return @num_possible_fills if finished?(remaining_buckets.size)
 
-    if remaining_to_fill == 0
-      @litres_to_fill += 1
-    elsif remaining_to_fill < 0
-      return
-    elsif remaining_to_fill > 0 && remaining_buckets.size > 0
-      binding.pry
-      # do_something_recursive
+    # for the remaining buckets, try all permutations from 1..n
+    (1..remaining_buckets.size).each do |combination_size|
+      remaining_buckets.combination(combination_size).each do |combination|
+        if remaining_to_fill - combination.reduce(:+) == 0
+          @num_possible_fills += 1
+        end
+      end
     end
+
+    first_bucket = buckets.pop
+    remaining_to_fill = litres_to_fill - first_bucket
+    return possible_fills(buckets, remaining_to_fill)
   end
 
   private
+
+
+  def finished?(size_remaining_buckets)
+    size_remaining_buckets == 0
+  end
+
 
   attr_reader :buckets, :litres_to_fill
 end
