2423bf5b4c8c2ac853356c7e76da8da45db8e2d8
Patrick Davey <patrick.davey@gmail.com>
2016-03-23 09:59:37 -0400

    part 1 of day19 complete

---
+++ day19/run.rb
@@ -0,0 +1,16 @@
+require './replacement.rb'
+require './molecule_calibrator.rb'
+require 'pry'
+
+replacements = []
+PARSE = /\A(?<start>\w+) => (?<transform>\w+)\z/
+IO.read("./input.txt").each_line do |replacement_string|
+  match = replacement_string.strip.match(PARSE)
+  replacements << Replacement.new(starting_molecule: match[:start],
+                                  inserted_molecule: match[:transform])
+end
+
+molecule_calibrator = MoleculeCalibrator.new(initial_molecule: "ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF",
+                                             replacements: replacements)
+
+puts molecule_calibrator.distinct_molecules
