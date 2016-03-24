62daeb618ec864dd70272634786ac96b951d4340
Patrick Davey <patrick.davey@gmail.com>
2016-03-24 10:33:57 -0400

    Refactoring to use divmod

--- day20/present_calculator.rb
+++ day20/present_calculator.rb
@@ -17,21 +17,14 @@ class PresentCalculator
 
 
   def presents_accum(house)
-    factors[house] = []
-
-    common_elf = (house.to_f / 2).ceil.downto(2).find do |elf|
-      house % elf == 0 && factors[elf]
+    factors = []
+    (house.to_f / 2).ceil.downto(1).each do |elf|
+      q, m = house.divmod(elf)
+      factors << q << elf if m == 0
     end
-
-    if common_elf
-      factors[house] = house * 10 + factors[common_elf]
-    else
-      factors[house] << 1 << house
-      factors[house] = factors[house].map { |elf| elf * 10 }.reduce(:+)
-    end
-
-    puts "total for house #{house} was #{factors[house]}" if house % 4000 == 0
-    factors[house]
+    total = factors.uniq.reduce(0) { |sum,e| sum + e*10}
+    puts "total: #{total} for house #{house}" if (house % 1000).zero?
+    total
   end
 
   attr_reader :number_presents, :house_checked_to, :factors
--- day20/run.rb
+++ day20/run.rb
@@ -3,7 +3,7 @@ require './present_calculator'
 MIN_PRESENTS = 34_000_000
 START_FROM = 1
 
-calc = PresentCalculator.new(MIN_PRESENTS, START_FROM)
+calc = PresentCalculator.new(MIN_PRESENTS)
 
 puts calc.house_number
 
