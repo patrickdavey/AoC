9c3d28fc2df401404d8cb808d73c814abda6cdee
Patrick Davey <patrick.davey@gmail.com>
2016-03-27 11:24:06 -0400

    Both scenarios passing now.
    Honestly, lots of random iterations seems like a
    fairly solid approach really

--- day22/wizard_finder_test.rb
+++ day22/wizard_finder_test.rb
@@ -10,4 +10,11 @@ class WizardFinderTest < Minitest::Test
     finder = WizardFinder.new(boss, wizard)
     assert_equal(173 + 53, finder.minimum_mana_after(10_000))
   end
+
+  def test_find_first_scenario_wizard
+    wizard = Wizard.new(10, 250)
+    boss = Boss.new(14, 8)
+    finder = WizardFinder.new(boss, wizard)
+    assert_equal(229 + 113 + 73 + 173 + 53, finder.minimum_mana_after(10_000))
+  end
 end
