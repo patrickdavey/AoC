e0765e4d2c94edceb3563e561e6026df486f4bb8
Patrick Davey <patrick.davey@gmail.com>
2016-03-27 14:21:56 -0400

    day 23 complete

--- day23/run.rb
+++ day23/run.rb
@@ -2,6 +2,6 @@ require "./turing.rb"
 require "./instruction_set.rb"
 
 parsed_instructions = InstructionSet.new(IO.read("./input.txt"))
-turing = Turing.new(parsed_instructions)
+turing = Turing.new(parsed_instructions, 1)
 turing.run!
 puts turing.register("b")
