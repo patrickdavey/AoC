226644cdd3114da674f6c8b7cffbd074241288ee
Patrick Davey <patrick.davey@gmail.com>
2016-03-28 13:37:32 -0400

    day 7 part 1 complete

--- day7/run.rb
+++ day7/run.rb
@@ -1,14 +1,5 @@
 require 'pry'
+require "./signal_finder.rb"
 
-signals = IO.readlines('test.txt')
-
-signals.each do |signal|
-  case
-  when match = signal.match(/^(?<value>\d+) -> (?<wire>\w+)/)
-  when match = signal.match(/^(?<first_input>.+) AND (?<second_input>.+) -> (?<wire>\w+)/)
-  when match = signal.match(/^(?<first_input>.+) OR (?<second_input>.+) -> (?<wire>\w+)/)
-  when match = signal.match(/^(?<first_input>.+) LSHIFT (?<second_input>.+) -> (?<wire>\w+)/)
-  when match = signal.match(/^(?<first_input>.+) RSHIFT (?<second_input>.+) -> (?<wire>\w+)/)
-  when match = signal.match(/^NOT (?<first_input>.+) -> (?<wire>\w+)/)
-  end
-end
+sf = SignalFinder.new(IO.readlines("input.txt"))
+puts sf.signal_on("a")
