0d315dfaa6478dc7f65b2c3631527b7fd444b2b0
Patrick Davey <patrick.davey@gmail.com>
2016-03-28 17:27:01 -0400

    correcting test for maximum price

--- day21/player_generator_test.rb
+++ day21/player_generator_test.rb
@@ -16,6 +16,6 @@ class PlayerGeneratorTest < Minitest::Test
   def test_multiple_players
     player_generator = PlayerGenerator.new(21)
     assert_equal 3, player_generator.count
-    assert player_generator.to_a[0].total_cost < player_generator.to_a[1].total_cost && player_generator.to_a[1].total_cost < player_generator.to_a[2].total_cost
+    assert player_generator.to_a[0].total_cost > player_generator.to_a[1].total_cost && player_generator.to_a[1].total_cost > player_generator.to_a[2].total_cost
   end
 end
