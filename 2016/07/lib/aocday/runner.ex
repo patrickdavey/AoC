ae1711f09109397f5befaba1488dbc2b97120296
Patrick Davey <patrick.davey@gmail.com>
2016-12-07 21:11:13 +0100

    use negative lookahead to remove some matches

--- 2016/07/lib/aocday/runner.ex
+++ 2016/07/lib/aocday/runner.ex
@@ -1,6 +1,6 @@
 defmodule AOCDay.Runner do
-  @bad_match ~r/\[\w*(\w)(\w)\2\1\w*]/
-  @good_match ~r/(?<first>\w)(?<second>\w)\2\1/
+  @bad_match ~r/\[\w*(\w)(?!\1)(\w)\2\1\w*]/
+  @good_match ~r/(?<first>\w)(?!\1)(?<second>\w)\2\1/
 
   def part_1 do
     structured_data
@@ -23,18 +23,17 @@ defmodule AOCDay.Runner do
   def valid_ipv7(string) do
     match1 = Regex.match?(@bad_match, string)
     match2 = Regex.named_captures(@good_match, string)
-    match1 == false && match2 && match2["first"] != match2["second"]
+    match1 == false && match2
   end
 
   def valid_ssl(string) do
     inside_brackets = Regex.scan(~r/(\[\w+])/, string, capture: :all_but_first) |> List.flatten
-    matches_outside_brackets = Regex.scan(~r/(?=(\w)(\w)\1(?![^[]*]))/, string, capture: :all_but_first)
+    matches_outside_brackets = Regex.scan(~r/(?=(\w)(?!\1)(\w)\1(?![^[]*]))/, string, capture: :all_but_first)
     Enum.filter(matches_outside_brackets, &find_match(&1, inside_brackets))
     |> Enum.count
     |> Kernel.>(0)
   end
 
-  def find_match([a, a], _ins), do: false
   def find_match([a, b], inside_brackets) do
     Enum.any?(inside_brackets, &(String.contains?(&1, "#{b}#{a}#{b}")))
   end
--- 2016/07/run.exs
+++ 2016/07/run.exs
@@ -1,2 +1,2 @@
-#IO.puts "part 1 : #{ AOCDay.Runner.part_1 }"
+IO.puts "part 1 : #{ AOCDay.Runner.part_1 }"
 IO.puts "part 2: #{ AOCDay.Runner.part_2 }"
