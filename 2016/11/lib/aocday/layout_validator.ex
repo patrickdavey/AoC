6f31b454ab9b0e09715194a22aaa00fcd932ac41
Patrick Davey <patrick.davey@gmail.com>
2016-12-11 14:36:57 +0100

    Board status checker functioning, I think.

--- 2016/11/lib/aocday/layout_validator.ex
+++ 2016/11/lib/aocday/layout_validator.ex
@@ -1,6 +1,39 @@
 defmodule AOCDay.LayoutValidator do
   alias AOCDay.Layout
 
-  def valid?(layout = %Layout{elevator: e}) when e > 3, do: false
-  def valid?(layout = %Layout{elevator: e}) when e < 0, do: false
+  def valid?(%Layout{elevator: e}) when e > 3, do: false
+  def valid?(%Layout{elevator: e}) when e < 0, do: false
+  def valid?(layout) do
+    [:floor_0, :floor_1, :floor_2, :floor_3]
+    |> Enum.map(&(Map.fetch!(layout, &1)))
+    |> Enum.all?(&floor_valid?/1)
+  end
+
+  defp floor_valid?([]), do: true
+  defp floor_valid?(floor) do
+    t = floor
+    |> Enum.reject(&(has_pair?(&1, floor)))
+    |> Enum.join
+    |> check
+  end
+
+  defp check(""), do: true
+  defp check(s) do
+    Regex.match?(~r/M.*G/, s) == false && Regex.match?(~r/G.*M/, s) == false
+  end
+
+  defp safe?(item, []), do: true
+  defp safe?(<<element::binary-size(1), "M"::binary>>, others) do
+    require IEx
+    IEx.pry
+  end
+  defp safe?(item, _), do: false
+
+  defp has_pair?(<<element::binary-size(1), "M"::binary>>, others) do
+    Enum.member?(others, element <> "G")
+  end
+  defp has_pair?(<<element::binary-size(1), "G"::binary>>, others) do
+    Enum.member?(others, element <> "M")
+  end
+  defp has_pair?(_,_), do: false
 end
--- 2016/11/test/layout_test.exs
+++ 2016/11/test/layout_test.exs
@@ -21,5 +21,50 @@ defmodule LayoutTest do
       floor_3: []
     }
     refute LayoutValidator.valid?(layout)
+
+    layout = %Layout{
+      elevator: 0,
+      floor_0: [],
+      floor_1: [],
+      floor_2: [],
+      floor_3: []
+    }
+    assert LayoutValidator.valid?(layout)
+
+    layout = %Layout{
+      elevator: 0,
+      floor_0: ["HM"],
+      floor_1: ["HG", "LM"],
+      floor_2: ["LG"],
+      floor_3: []
+    }
+    refute LayoutValidator.valid?(layout)
+
+    layout = %Layout{
+      elevator: 0,
+      floor_0: ["HM", "HG", "LM"],
+      floor_1: [],
+      floor_2: ["LG"],
+      floor_3: []
+    }
+    assert LayoutValidator.valid?(layout)
+
+    layout = %Layout{
+      elevator: 0,
+      floor_0: [],
+      floor_1: [],
+      floor_2: [],
+      floor_3: ["HM", "HG", "LM", "LG"]
+    }
+    assert LayoutValidator.valid?(layout)
+
+    layout = %Layout{
+      elevator: 0,
+      floor_0: ["JM", "QM", "BM"],
+      floor_1: ["AG", "BG"],
+      floor_2: [],
+      floor_3: ["HM", "HG", "LM", "LG"]
+    }
+    assert LayoutValidator.valid?(layout)
   end
 end
