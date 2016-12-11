57e30651f6d75991ef11a6586482552171845031
Patrick Davey <patrick.davey@gmail.com>
2016-12-11 16:06:21 +0100

    I think the generation code is working OK

--- 2016/11/test/layout_generator_test.exs
+++ 2016/11/test/layout_generator_test.exs
@@ -51,4 +51,47 @@ defmodule LayoutGeneratorTest do
       floor_3: []
     })
   end
+
+  test "third part works" do
+    layout = %Layout{
+      elevator: 2,
+      floor_0: ["LM"],
+      floor_1: [],
+      floor_2: ["HG", "HM", "LG"],
+      floor_3: []
+    }
+    nodes = LayoutGenerator.nodes(layout)
+
+    assert Enum.member?(nodes, %Layout{
+      elevator: 1,
+      floor_0: ["LM"],
+      floor_1: [ "HM"],
+      floor_2: ["HG", "LG"],
+      floor_3: []
+    })
+
+    assert Enum.member?(nodes, %Layout{
+      elevator: 1,
+      floor_0: ["LM"],
+      floor_1: [ "HG", "HM"],
+      floor_2: ["LG"],
+      floor_3: []
+    })
+
+    assert Enum.member?(nodes, %Layout{
+      elevator: 3,
+      floor_0: ["LM"],
+      floor_1: [],
+      floor_2: ["LG"],
+      floor_3: [ "HG", "HM"]
+    })
+
+    assert Enum.member?(nodes, %Layout{
+      elevator: 1,
+      floor_0: ["LM"],
+      floor_1: [ "LG" ],
+      floor_2: ["HG", "HM"],
+      floor_3: []
+    })
+  end
 end
