35701654b9116ed45b216ca8b57fca4896c3a4eb
Patrick Davey <patrick.davey@gmail.com>
2016-12-19 16:17:49 +0100

    reworked part 1

--- 2016/19/lib/aocday/runner.ex
+++ 2016/19/lib/aocday/runner.ex
@@ -1,14 +1,19 @@
 defmodule AOCDay.Runner do
   def who_has_all_presents?(number_elves) do
     elves_in_game = Enum.to_list(1..number_elves)
-    last_elf_standing(elves_in_game)
+    last_elf_standing(elves_in_game, [])
   end
 
-  def last_elf_standing([h | []]), do: h
+  def last_elf_standing([], [e]), do: e
+  def last_elf_standing([elf], rest) do
+    last_elf_standing([elf | Enum.reverse(rest)], [])
+  end
 
-  def last_elf_standing(elves_in_game = [h | t]) do
-    elves = List.replace_at(elves_in_game, div(length(elves_in_game), 2), nil)
-    elves = List.delete_at(elves, 0)
-    last_elf_standing(List.insert_at(elves, -1, h))
+  def last_elf_standing([], rest) do
+    last_elf_standing(Enum.reverse(rest), [])
+  end
+
+  def last_elf_standing([a, b | t], rest) do
+    last_elf_standing(t, [a | rest])
   end
 end
