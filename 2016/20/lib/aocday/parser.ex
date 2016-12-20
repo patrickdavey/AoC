6a8d3be8c7269710a0302b7af6e827e696990253
Patrick Davey <patrick.davey@gmail.com>
2016-12-20 08:14:57 +0100

    parsing working, I think

--- 2016/20/lib/aocday/parser.ex
+++ 2016/20/lib/aocday/parser.ex
@@ -11,17 +11,58 @@ defmodule AOCDay.Parser do
   end
 
   defp _parse(binary) do
-    binary
+    initial = binary
     |> String.trim
     |> String.split("\n")
     |> Enum.map(&formatted/1)
+    |> Enum.reduce(MapSet.new, &accumulate_ranges/2)
+
+    reduce_further(initial, MapSet.size(initial))
   end
 
   def formatted(line) do
     [a, b] = line
     |> String.split("-")
+    |> Enum.map(&(String.trim(&1)))
     |> Enum.map(&(String.to_integer(&1)))
 
     a..b
+  end
+
+  defp accumulate_ranges(a..b, mapset) do
+    acc = MapSet.to_list(mapset) # there must be a nicer way to do this, can't think
+    start_overlap = Enum.find(acc, &(Enum.member?(&1, a)))
+    end_overlap = Enum.find(acc, &(Enum.member?(&1, b)))
+    cond do
+      start_overlap == nil && end_overlap == nil ->
+        MapSet.put(mapset, a..b)
+      start_overlap != nil && end_overlap == nil ->
+        start_over..end_over = start_overlap
+        mapset = MapSet.delete(mapset, start_overlap)
+        MapSet.put(mapset, start_over..b)
+      start_overlap == nil && end_overlap != nil ->
+        # we know that b is inside the end of a range,
+        # therefore we extend the _start_ of that range by a
+        start_over..end_over = end_overlap
+        mapset = MapSet.delete(mapset, end_overlap)
+        MapSet.put(mapset, a..end_over)
+      :otherwise ->
+        if (a..b) == start_overlap && (a..b) == end_overlap do
+          mapset
+        else
+          MapSet.delete(mapset, a..b)
+        end
+    end
+  end
+
+  defp reduce_further(mapset, size) do
+    list = MapSet.to_list(mapset)
+    next = Enum.reduce(list, mapset, &accumulate_ranges/2)
+
+    if MapSet.size(next) == size do
+      mapset
+    else
+      reduce_further(next, MapSet.size(next))
+    end
   end
 end
--- 2016/20/test/parser_test.exs
+++ 2016/20/test/parser_test.exs
@@ -4,12 +4,47 @@ defmodule RunnerTest do
 
   test "returns ranges only" do
     input = "
-5-8
-0-2
-4-7
-"
+      5-8
+      0-2
+      4-7
+      "
     parsed = Parser.parse(input)
     assert Enum.count(parsed) == 2
     assert Enum.member?(parsed, 4..8)
+  end
+
+  test "returns ranges only high overlap" do
+    input = "
+      4-7
+      0-2
+      5-8
+      "
+    parsed = Parser.parse(input)
+    assert Enum.count(parsed) == 2
+    assert Enum.member?(parsed, 4..8)
+  end
+
+  test "removes an overlapped range" do
+    input = "
+      3-9
+      4-7
+      0-2
+      "
+    parsed = Parser.parse(input)
+    assert Enum.count(parsed) == 2
+
+    assert Enum.member?(parsed, 3..9)
+  end
+
+  test "replaces an entire range" do
+    input = "
+      4-7
+      0-2
+      3-9
+      "
+    parsed = Parser.parse(input)
+    assert Enum.count(parsed) == 2
+
+    assert Enum.member?(parsed, 3..9)
   end
 end
