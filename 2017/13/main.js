a40ae76e016365cc6f02ff57e007d0d712b29ddc
Patrick Davey <patrick.davey@gmail.com>
2017-12-14 07:56:20 +1300

    slow, but works

--- 2017/13/main.js
+++ 2017/13/main.js
@@ -3,6 +3,8 @@ import { parse } from "./lib/parser";
 import { part1, part2 } from "./lib/runner";
 
 let parsed = parse(raw);
-//console.log(part1(parsed));
+console.log(part1(parsed));
+
+parsed = parse(raw);
 console.log(part2(parsed));
 
