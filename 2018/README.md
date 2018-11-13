ffa59815a9cd42cef2a3ee49a5fea8036abba4ed
Patrick Davey <patrick.davey@gmail.com>
2018-11-13 15:50:48 +1300

    basic test outline created

---
+++ 2018/README.md
@@ -0,0 +1,3 @@
+Not attempting to win any speed or style records here. Simply attempting to go from zero to not quite zero with python.
+
+Do _NOT_ look at this repo and attempt to learn anything about how to write python nicely.
---
+++ 2018/get_input.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+cd /Users/patrickdavey/sites/advent_of_code/2018
+# downloads instructions and input locally for a given day
+# assumes your directory name is the number of adventday
+
+if [[ $1 -eq 0 ]] ; then
+  echo 'please supply the day (1-25, not zero based): (d)'
+  exit 0
+fi
+
+DAY=$1
+DIR="./${DAY}/"
+rsync -va "/Users/patrickdavey/sites/advent_of_code/2018/common/"* "${DIR}"
+cd "${DIR}"
+curl -b session=$(cat ${HOME}/.aocrc) http://adventofcode.com/2018/day/${DAY}/input > input.txt
+curl -b session=$(cat ${HOME}/.aocrc) http://adventofcode.com/2018/day/${DAY} > instructions.html
+pandoc -s -r html instructions.html -o temp.md
+rm instructions.html
+sed -n "/^--- Day $DAY/,\$p" temp.md > instructions.md
+rm temp.md
