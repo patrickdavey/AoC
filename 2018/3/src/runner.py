ed72ec2966394d2c7f55893527ead34c804465d6
Patrick Davey <patrick.davey@gmail.com>
2018-12-04 07:58:19 +1300

    small refactor

--- 2018/3/src/runner.py
+++ 2018/3/src/runner.py
@@ -1,18 +1,20 @@
 import re
-from collections import defaultdict
+from collections import defaultdict, namedtuple
+
+Claim = namedtuple('Claim', 'id x y width height')
 
 def parse(raw):
   lines = [line.strip() for line in raw.split("\n")]
   parsed = [re.search("#(\d+) @ (\d+),(\d+): (\d+)x(\d+)", line).groups() for line in lines]
-  return [list(map(int, line)) for line in parsed]
+  return [Claim(*list(map(int, line))) for line in parsed]
 
 def run(raw):
   claims = parse(raw)
   d = defaultdict(int)
 
-  for (id, x, y, width, height) in claims:
-    for x_pos in range(x, x + width):
-      for y_pos in range(y, y + height):
+  for claim in claims:
+    for x_pos in range(claim.x, claim.x + claim.width):
+      for y_pos in range(claim.y, claim.y + claim.height):
         d[(x_pos, y_pos)] += 1
   return len([i for i in d.values() if i >= 2])
 
@@ -33,3 +35,15 @@ def run2(raw):
     if id not in more_than_one_claim:
       return id
 
+
+### elegant solution from https://www.reddit.com/r/adventofcode/comments/a2lesz/2018_day_3_solutions/eazoznn
+# import re
+# from collections import Counter
+
+# claims = [[*map(int, re.findall(r'\d+', l))] for l in input.splitlines() if l]
+# squares = lambda c: ((i, j) for i in range(c[1], c[1]+c[3])
+#                             for j in range(c[2], c[2]+c[4]))
+# fabric = Counter(s for c in claims for s in squares(c))
+
+# part1 = sum(1 for v in fabric.values() if v > 1)
+# part2 = next(c[0] for c in claims if all(fabric[s] == 1 for s in squares(c)))
