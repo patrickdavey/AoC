acb5c2f34f4e316f20e9fc44daae61f21821434c
Patrick Davey <patrick.davey@gmail.com>
2018-12-03 13:35:51 +1300

    Refactoring based on reddit.
    Far nicer, I like the use of combinations (no doubt that'll come in
    handy later). The compress function is nice too.
    
    See https://www.reddit.com/r/adventofcode/comments/a2aimr/2018_day_2_solutions/

--- 2018/2/src/runner.py
+++ 2018/2/src/runner.py
@@ -1,4 +1,5 @@
 from grouper import group_count
+from itertools import combinations, compress
 
 def run(input):
   ids = [line.strip() for line in input.splitlines()]
@@ -11,24 +12,9 @@ def run(input):
     threes += three
   return twos * threes
 
-def find_match(letters):
-  for string in letters:
-    for other in letters:
-      diff = 0
-      for first, second in zip(string, other):
-        if first != second:
-          diff += 1
-
-      if diff == 1:
-        return(string, other)
-
 def run2(input):
   ids = [line.strip() for line in input.splitlines()]
-  letters = [[char for char in line] for line in ids]
-
-  (a, b) = find_match(letters)
-  acc = []
-  for first, second in zip(a, b):
-    if first == second:
-      acc.append(first)
-  return "".join(acc)
+  for (first, second) in combinations(ids, 2):
+    diff = [c1 == c2 for (c1, c2) in zip(first, second)]
+    if sum(diff) == (len(first) - 1):
+      return "".join(compress(first, diff))
