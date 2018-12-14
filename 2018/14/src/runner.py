a23b24c24a3d16182616813c6c9da367e6d286b1
Patrick Davey <patrick.davey@gmail.com>
2018-12-14 19:57:06 +1300

    wip

---
+++ 2018/14/src/runner.py
@@ -0,0 +1,9 @@
+class Runner():
+  def __init__(self, raw):
+    self.raw = raw
+
+  def part1(self):
+    return True
+
+  def part2(self):
+    return True
---
+++ 2018/14/tests/runner_test.py
@@ -0,0 +1,17 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from runner import Runner
+
+test_input = """
+foo
+"""
+
+class RunnerTest(unittest.TestCase):
+  def test_part1(self):
+    self.assertEqual(Runner(test_input.strip()).part1(), True)
+
+  def test_part2(self):
+    self.assertEqual(Runner(test_input.strip()).part2(), True)
