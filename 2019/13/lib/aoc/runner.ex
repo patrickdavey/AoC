00cc9deea581248b291a5d505b7261410e1ab859
Patrick Davey <patrick.davey@gmail.com>
2020-01-23 09:21:42 +1300

    Day 13 part 1 complete

---
+++ 2019/13/lib/aoc/runner.ex
@@ -0,0 +1,40 @@
+defmodule AOC.Runner do
+  @block_type 2
+  alias AOC.{IntcodeAgent, Board}
+
+
+  def part_1(program \\ structured_data()) do
+    supervisor_pid = self()
+    computer = spawn_link(fn -> IntcodeAgent.init(%{supervisor: supervisor_pid}) end)
+    send(computer, {:set_initial, self(), program})
+    send(computer, :run)
+
+    wait_loop(%{}, computer, [])
+    |> Map.values
+    |> Enum.count(&(&1 == @block_type))
+  end
+
+  defp structured_data do
+    AOC.Parser.parse
+  end
+
+  defp wait_loop(board, computer, acc) when length(acc) < 2 do
+    receive do
+      {:terminating, ^computer} ->
+        board
+      {:input, value, ^computer} ->
+        wait_loop(board, computer, acc ++ [value])
+    end
+  end
+
+  defp wait_loop(board, computer, [x, y]) do
+    receive do
+      {:terminating, ^computer} ->
+        board
+      {:input, type, ^computer} ->
+        board
+        |> Map.put({x, y}, type)
+        |> wait_loop(computer, [])
+    end
+  end
+end
