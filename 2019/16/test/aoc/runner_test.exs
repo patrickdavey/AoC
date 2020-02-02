6b83f59871887b044a448c558bce1cc41523fc5e
Patrick Davey <patrick.davey@gmail.com>
2020-02-03 09:00:22 +1300

    wip

---
+++ 2019/16/test/aoc/runner_test.exs
@@ -0,0 +1,11 @@
+defmodule AOCRunnerTest do
+  use ExUnit.Case
+  alias AOC.{Runner, Parser}
+
+  test "first sample runs correctly" do
+    assert(Runner.part_2(Parser.parse("12345678"), 1) == 48226158)
+    assert(Runner.part_2(Parser.parse("12345678"), 2) == 34040438)
+    assert(Runner.part_2(Parser.parse("12345678"), 3) == 03415518)
+    assert(Runner.part_2(Parser.parse("12345678"), 4) == 01029498)
+  end
+end
