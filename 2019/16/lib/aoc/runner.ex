96cc23ce33b60a048a1f71e730b4b4d1633b50ea
Patrick Davey <patrick.davey@gmail.com>
2020-02-03 14:59:44 +1300

    wip

--- 2019/16/lib/aoc/runner.ex
+++ 2019/16/lib/aoc/runner.ex
@@ -1,7 +1,11 @@
 defmodule AOC.Runner do
-  def part_1(data \\ structured_data(), iterations) do
+  @pattern [0, 1, 0, -1]
+  def part_1(inputs \\ structured_data(), iterations) do
+    offsets_per_input = calculate_offsets(inputs |> length)
     require IEx
     IEx.pry
+
+
   end
 
   def part_2 do
@@ -10,5 +14,13 @@ defmodule AOC.Runner do
 
   defp structured_data do
     AOC.Parser.parse
+  end
+
+  defp calculate_offsets(length) do
+    foo = Enum.map(1..length, fn(index) ->
+      List.duplicate(@pattern, index) |> Enum.zip |> Enum.map(&(Tuple.to_list/1)) |> List.flatten |> Stream.cycle |> Enum.take(length + 1) |> List.delete_at(0)
+    end)
+    require IEx
+    IEx.pry
   end
 end
