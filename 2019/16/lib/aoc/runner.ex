465ace1bd7ee82e01231627941c8f4b3ac1d2e3f
Patrick Davey <patrick.davey@gmail.com>
2020-02-03 15:08:10 +1300

    wip

--- 2019/16/lib/aoc/runner.ex
+++ 2019/16/lib/aoc/runner.ex
@@ -1,11 +1,9 @@
 defmodule AOC.Runner do
   @pattern [0, 1, 0, -1]
   def part_1(inputs \\ structured_data(), iterations) do
-    offsets_per_input = calculate_offsets(inputs |> length)
-    require IEx
-    IEx.pry
+    offsets = calculate_offsets(inputs |> length)
 
-
+    calculate(inputs, offsets, iterations)
   end
 
   def part_2 do
@@ -17,10 +15,19 @@ defmodule AOC.Runner do
   end
 
   defp calculate_offsets(length) do
-    foo = Enum.map(1..length, fn(index) ->
+    Enum.map(1..length, fn(index) ->
       List.duplicate(@pattern, index) |> Enum.zip |> Enum.map(&(Tuple.to_list/1)) |> List.flatten |> Stream.cycle |> Enum.take(length + 1) |> List.delete_at(0)
     end)
-    require IEx
-    IEx.pry
+  end
+
+  defp calculate(inputs, offset, 0) do
+    inputs |> Enum.join |> String.to_integer
+  end
+
+  defp calculate(inputs, offsets, n) do
+    Enum.map(offsets, fn(o) ->
+      Enum.zip(inputs, o) |> Enum.map(fn({a, b}) -> a * b end) |> Enum.sum |> abs |> Integer.digits |> List.last
+    end)
+    |> calculate(offsets, n - 1)
   end
 end
