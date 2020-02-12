0bec56d0e02225a0464e0c8b766977fed2f6212e
Patrick Davey <patrick.davey@gmail.com>
2020-02-13 09:57:02 +1300

    wip

--- 2019/17/lib/aoc/runner.ex
+++ 2019/17/lib/aoc/runner.ex
@@ -1,13 +1,30 @@
 defmodule AOC.Runner do
-  def part_1 do
-    "part 1"
-  end
+  alias AOC.{IntcodeAgent, Board}
 
-  def part_2 do
-    "part 2"
+  def part_1(program \\ structured_data()) do
+    supervisor_pid = self()
+    intcode = spawn_link(fn -> IntcodeAgent.init(%{supervisor: supervisor_pid}) end)
+    Process.register(intcode, :intcode)
+    send(intcode, {:set_initial, self(), program})
+    send(intcode, :run)
+
+    wait_loop(%{}, intcode)
   end
 
   defp structured_data do
     AOC.Parser.parse
+  end
+
+  defp wait_loop(board, intcode) do
+    receive do
+      {:terminating, ^intcode} ->
+        require IEx
+        IEx.pry
+        board
+      {:input, value, ^intcode} ->
+        require IEx
+        IEx.pry
+        wait_loop(board, intcode)
+    end
   end
 end
